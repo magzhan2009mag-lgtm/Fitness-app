b0cf9a1c385af24472c87c453a7560d648d96cbc
m1go <236520538+magzhan2009mag-lgtm@users.noreply.github.com>
2026-09-15 12:36:00 +0500

    Deploy Web Build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1670760678" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3352353325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
